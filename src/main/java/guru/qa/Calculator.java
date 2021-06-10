3969abc761b83ac4969e05594d24900393d81111
PercyGB <kuznetsovalv@yandex.ru>
2021-06-10 12:06:17 +0400

    3 lines commit

--- src/main/java/guru/qa/Calculator.java
+++ src/main/java/guru/qa/Calculator.java
@@ -2,6 +2,8 @@ package guru.qa;
 
 public class Calculator {
     public void start(){
-
+        System.out.println("started");
+        System.out.println("2 line");
+        System.out.println("stopped");
     }
 }
